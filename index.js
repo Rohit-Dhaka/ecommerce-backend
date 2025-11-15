99ad7e8bc4964e4511eabfe700e9bff55638c400
Rohit dhaka <rohitdhaka@gmail.com>
2025-11-15 16:13:29 +0530

    change port

--- index.js
+++ index.js
@@ -3,25 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import ConnectDB from "./config/db.js";
 import router from "./routes/index.js";
-
-
 dotenv.config();
-
 const app = express();
-
-
 const PORT = process.env.PORT || 4000;
-
 
 app.use(express.json());
 app.use(cors());
-
-
 app.use("/api/v1", router);
-
-
 ConnectDB();
-
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
